fda050bf537c57e6fa7dee9325bfdf9c4f2b4d57
Eduardo <eduardokunmixyeah@gmail.com>
2024-04-16 23:56:39 -0400

    add logo, change title, fix login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,29 +1,57 @@
 
-import { Button } from 'primereact/button';
 import { InputText } from "primereact/inputtext";
 import { Password } from 'primereact/password';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+
+interface UserLogin {
+    name: string;
+    password: string;
+}
+
+type UserKeys = 'name' | 'password';
 
 export const Login = () => {
-    const { register, handleSubmit } = useForm();
+    
+    const defaultValues: UserLogin = {
+        name: '',
+        password: '',
+    }
+
+    const { control, formState: { errors }, handleSubmit } = useForm({ defaultValues });
+
+    const getFormErrorMessage = (name: string) => {
+        return errors[name as UserKeys] && <small className=" text-white">{errors[name as UserKeys]?.message}</small>
+    };
+
+    const onSubmit = (data: UserLogin) => {
+        console.log(data);
+    };
 
     return (
-        <div className='flex flex-col items-center justify-center gap-5 bg-blue-500 rounded-2xl shadow-2xl p-16 '>
+        <div className='flex flex-col items-center justify-center gap-5 bg-blue-600 rounded-2xl shadow-2xl w-96 h-auto py-8'>
             <h1 className='text-2xl font-bold'>Login</h1>
 
-            <form onSubmit={handleSubmit(data => console.log(data))}>
+            <div className="flex items-center justify-center w-[482px] h-full"></div>
+
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="flex flex-col items-start justify-center h-[10rem]">
-                    <label className=' text-white ml-4 mb-2'>Username</label>
-                    <InputText {...register('username')} />
+                    <label className=' text-white ml-2 mb-2'>Username</label>
+                    <Controller name="name" control={control} rules={{ required: 'Name is required.' }} render={({ field }) => (
+                        <InputText id={field.name} {...field} autoFocus/>
+                    )} />
+                    {getFormErrorMessage('name')}
                 </div>
 
                 <div className="flex flex-col items-start justify-center h-[10rem]">
-                    <label className=' text-white ml-4 mb-2'>Password</label>
-                    <Password {...register('password')} feedback={false} toggleMask/>
+                    <label className=' text-white ml-2 mb-2'>Password</label>
+                    <Controller name="password" control={control} rules={{ required: 'Hola' }} render={({ field }) => (
+                        <Password id={field.name} {...field} toggleMask feedback={false}/>
+                    )} />
+                    {getFormErrorMessage('password')}
                 </div>
 
-                <div className="flex items-center justify-center">
-                    <Button label="Iniciar sesión" severity="secondary"  type='submit' />
+                <div className="flex items-center justify-center gap-5">
+                    <button type='submit' className=' bg-gray-500 hover:bg-gray-600 transition-all text-white rounded-md shadow-2xl p-4'>Iniciar sesión</button>
                 </div>
             </form>
         </div>
